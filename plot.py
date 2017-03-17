5ddc0f52795a1437c71f4c6686b626634bf5ae67
dbxnr <kevinmalachimulligan@gmail.com>
2017-03-17 21:15:19 +0000

    requirements.txt

--- plot.py
+++ plot.py
@@ -33,6 +33,7 @@ class MonitorStats:
         self.win.resize(800,600)
         self.plot = self.win.addPlot(title='CPU and Swap Mem Usage', axisItems={'bottom': TimeAxisItem(orientation='bottom')})
         self.plot.addLegend()
+        self.plot.setLimits(yMin=0, yMax=100, xMin=0) # Adds some limits to the size of the graph canvas
         self.plot.showGrid(x=True,y=True)
         self.plot.setLabel('left', "Percentage Utilization")
         self.cpu_stats = self.plot.plot(pen='r',name="CPU Usage")
